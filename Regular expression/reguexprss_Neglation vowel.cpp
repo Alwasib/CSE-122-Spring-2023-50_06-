52bf14fc2d6e1f0c597d0043e244da9dd3a9b933
Al- Wasib <122206303+Alwasib@users.noreply.github.com>
2024-01-28 22:09:10 +0600

    Add files via upload

---
+++ Regular expression/reguexprss_Neglation vowel.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int i,l,c=0;
+   string s;
+   cin>>s;
+   l=s.size();
+   for(i=0;i<l;i++)
+   {
+       if( s[i]=='a'|| s[i]=='e'|| s[i]=='o'|| s[i]=='u'|| s[i]=='i')
+       {
+          c++;
+       }
+
+   }
+   if(c>0)
+   {
+        cout<<"INVALID"<<endl;
+   }
+   else
+   {
+        cout<<"VALID"<<endl;
+   }
+}
